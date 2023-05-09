afa00cea6c5a5fe48f3d98fd90f5e46e124f6d88
Konstantin <nemeshkv@gmail.com>
2023-05-09 23:29:37 +0500

    proverka i vivisti url

---
+++ src/ru/nemesh/Check.java
@@ -0,0 +1,14 @@
+package ru.nemesh;
+
+public class Check {
+    static boolean getCheckfio(String a, String c) {
+//        String[] mas = input.split("");
+//        String a = String.valueOf(mas.length);
+//        for (int i = 0; i < a.length(); i++) {
+        if (c.equals("a")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/ru/nemesh/Poisk.java
+++ src/ru/nemesh/Poisk.java
@@ -1,36 +1,79 @@
 package ru.nemesh;
 
 import org.jsoup.Jsoup;
-import sun.security.ssl.BaseSSLSocketImpl;
+//import sun.security.ssl.BaseSSLSocketImpl;
 
 import javax.lang.model.util.Elements;
 import javax.swing.text.html.parser.Element;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
 
 public class Poisk {
     public static String poiskfio(String a) throws Exception {
-        String google = "http://www.google.com/search?q=";
+        String google = "https://www.google.com/search?q=";
         String search = a;
         String charset = "UTF-8";
-        String userAgent = "ExampleBot 1.0 (+http://example.com/bot)"; // Change this to your company's name and bot homepage!
+        String c = null;
+        String result = null;
 
-        Elements links = (Elements) Jsoup.connect(google + URLEncoder.encode(search, charset)).userAgent(userAgent).get().select("li.g>h3>a");
+        URL url = new URL("https://www.google.com/search?q=" + a);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        URLConnection connection = url.openConnection();
+        connection.setDoOutput(true);
 
-        for (Element link : links) {
-            String title = link.text();
-            String url = link.absUrl("href"); // Google returns URLs in format "http://www.google.com/url?q=<url>&sa=U&ei=<someKey>".
-            url = URLDecoder.decode(url.substring(url.indexOf('=') + 1, url.indexOf('&')), "UTF-8");
 
-            if (!url.startsWith("http")) {
-                continue; // Ads/news/etc.
-            }
-
-            System.out.print(url);
-            System.out.print("\t\t\t");
-            System.out.println(title);
-
-            return a;
+        try {
+            OutputStream output = connection.getOutputStream();
+            PrintStream sender = new PrintStream(output);
+            sender.println(a);
+        } catch (ConnectException e) {
+            System.out.println(e.getMessage());
         }
+
+        try {
+            InputStream input = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            while (reader.ready()) {
+
+                c = reader.readLine();
+                result = String.valueOf(Check.getCheckfio(a,c));
+                if (result == String.valueOf(true));
+                return result;
+             //   return c;
+
+                //надо седалать вывод только найденных строк (url) где есть значение
+            }
+        } catch (ConnectException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+        //return c;
+
+        //  result = JsonConvert.convertJson(c);
     }
 }
+
+
+//        String userAgent = "ExampleBot 1.0 (+http://example.com/bot)"; // Change this to your company's name and bot homepage!
+//
+//        Elements links = (Elements) Jsoup.connect(google + URLEncoder.encode(search, charset)).userAgent(userAgent).get().select("li.g>h3>a");
+//
+//        for (Element link : links) {
+//            String title = link.text();
+//            String url = link.absUrl("href"); // Google returns URLs in format "http://www.google.com/url?q=<url>&sa=U&ei=<someKey>".
+//            url = URLDecoder.decode(url.substring(url.indexOf('=') + 1, url.indexOf('&')), "UTF-8");
+//
+//            if (!url.startsWith("http")) {
+//                continue; // Ads/news/etc.
+//            }
+//
+//            System.out.print(url);
+//            System.out.print("\t\t\t");
+//            System.out.println(title);
+//
+//            return a;
+//        }
+//    }
+//}
